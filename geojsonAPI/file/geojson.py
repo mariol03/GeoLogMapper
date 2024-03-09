f2cb83c15efcc6eb1017ac893f5553e303270363
Carlos Sesma <41452910+usarral@users.noreply.github.com>
2024-03-09 20:21:11 +0100

    Give chmod +x

--- geojsonAPI/file/geojson.py
+++ geojsonAPI/file/geojson.py
@@ -3,21 +3,22 @@ import json
 from flask import Flask
 
 app = Flask("__name__")
+script = "/usr/bin/getgeojson.sh"
 
 @app.route("/get10m")
 def get10m():
-    subprocess.run(["bash", "/usr/bin/getgeojson.sh", "10m"],stdout = subprocess.DEVNULL, stderr = subprocess.DEVNULL)
+    subprocess.run(["bash", script, "10m"],stdout = subprocess.DEVNULL, stderr = subprocess.DEVNULL)
     file = json.loads(open("ne_10m_admin_0_countries.geo.json","r").read())
     return file
 
 @app.route("/get50m")
 def get50m():
-    subprocess.run(["bash", "/usr/bin/getgeojson.sh", "50m"],stdout = subprocess.DEVNULL, stderr = subprocess.DEVNULL)
+    subprocess.run(["bash", script, "50m"],stdout = subprocess.DEVNULL, stderr = subprocess.DEVNULL)
     file = json.loads(open("ne_50m_admin_0_countries.geo.json","r").read())
     return file
 
 @app.route("/get110m")
 def get110m():
-    subprocess.run(["bash", "/usr/bin/getgeojson.sh", "110m"],stdout = subprocess.DEVNULL, stderr = subprocess.DEVNULL)
+    subprocess.run(["bash", script, "110m"],stdout = subprocess.DEVNULL, stderr = subprocess.DEVNULL)
     file = json.loads(open("ne_110m_admin_0_countries.geo.json","r").read())
     return file
